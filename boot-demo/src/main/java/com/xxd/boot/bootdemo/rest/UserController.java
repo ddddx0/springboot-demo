b62290e73b28ed72185cd97a75bfdeffc825975c
xiexudong <daniel_842791908@qq.com>
2018-08-06 22:45:54 +0800

    添加druid连接池，未完成

---
+++ boot-demo/src/main/java/com/xxd/boot/bootdemo/rest/UserController.java
@@ -0,0 +1,13 @@
+package com.xxd.boot.bootdemo.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping("all")
+    public String getAll(){
+        return "";
+    }
+}
