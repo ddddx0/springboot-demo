d0dd4282a46c6991c198eb50fdee514241124d3f
xiexudong <daniel_842791908@qq.com>
2018-09-08 13:46:24 +0800

    jenkins测试14：webhook

--- boot-demo/src/main/java/com/xxd/boot/bootdemo/rest/UserController.java
+++ boot-demo/src/main/java/com/xxd/boot/bootdemo/rest/UserController.java
@@ -1,5 +1,6 @@
 package com.xxd.boot.bootdemo.rest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,12 @@ public class UserController {
     }
 
     @RequestMapping("/test")
-    public String test(){
+    public String test() {
         return "HelloWorld";
+    }
+
+    @GetMapping("/test1")
+    public String test1() {
+        return "success";
     }
 }
